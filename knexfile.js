27ed0a7c6f4d768ef3073056b64a313c23522b1a
jafdel <20396066@myclyde.ac.uk>
2025-02-28 17:07:03 +0000

    knexfile and fly.toml configs

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      connectionString: 'postgres://postgres:CBfYU8Ky2TWhFiv@facerecogbrain-express-mm41ea-db.flycast:5432'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
